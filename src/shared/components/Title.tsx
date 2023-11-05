e0a6b27d2a0c739b6c35b312ffbe3e44eda0a643
dzulfiqarzakyRaiz <zaky.aulia@raizinvest.com.au>
2023-11-06 06:51:57 +0700

    install babel

--- src/shared/components/Title.tsx
+++ src/shared/components/Title.tsx
@@ -9,16 +9,14 @@ type TitleComponentProps = {
     title: string[];
 };
 
-const Title = memo(({ title }: TitleComponentProps) => {
-    return (
-        <IntroMotion start={ANIMATION_DIRECTION.RIGHT} end={ANIMATION_DIRECTION.BOTTOM}>
-            <TitleStyled>
-                {title.map((letter, index) => (
-                    <BounceMotion key={index}>{letter === COMMON.EMPTY_SPACE ? COMMON.SPACE : letter}</BounceMotion>
-                ))}
-            </TitleStyled>
-        </IntroMotion>
-    );
-});
+const Title = memo(({ title }: TitleComponentProps) => (
+    <IntroMotion start={ANIMATION_DIRECTION.RIGHT} end={ANIMATION_DIRECTION.BOTTOM}>
+        <TitleStyled>
+            {title?.map((letter, index) => (
+                <BounceMotion key={index}>{letter === COMMON.EMPTY_SPACE ? COMMON.SPACE : letter}</BounceMotion>
+            ))}
+        </TitleStyled>
+    </IntroMotion>
+));
 
 export default Title;
